06d3542a2417d2a978e9f7904a5db473790e4a0f
phwnganh <phuonganh25102003@gmail.com>
2026-04-13 17:09:03 +0700

    fix: hover the sidebar footer to easily click the dropdown

--- src/layouts/components/header/HeaderAction.tsx
+++ src/layouts/components/header/HeaderAction.tsx
@@ -3,11 +3,11 @@ import LanguageIcon from '@/assets/icons/language-icon.svg?react'
 const HeaderAction = () => {
     return (
         <div className={"flex items-center gap-6 pr-8"}>
-            <button type={"button"} aria-label={"open to view notification"} className={"flex justify-center items-center w-6 h-6 shrink-0"}>
-                <NotificationIcon/>
+            <button type={"button"} aria-label={"open notifications"} className={" hover:bg-bg-tertiary p-2 rounded-md transition"}>
+                <NotificationIcon className={"w-6 h-6"}/>
             </button>
-            <button type={"button"} aria-label={"click to change another language"} className={"flex justify-center items-center w-6 h-6 shrink-0"}>
-                <LanguageIcon/>
+            <button type={"button"} aria-label={"change language"} className={"hover:bg-bg-tertiary p-2 rounded-md transition"}>
+                <LanguageIcon className={"w-6 h-6"}/>
             </button>
         </div>
     );
