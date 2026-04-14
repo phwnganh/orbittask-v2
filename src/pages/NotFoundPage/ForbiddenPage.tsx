aea52afdb969d88aa62ad300755ec37bba34b264
phwnganh <phuonganh25102003@gmail.com>
2026-04-14 16:45:33 +0700

    fix: ui of forbidden page

--- src/pages/NotFoundPage/ForbiddenPage.tsx
+++ src/pages/NotFoundPage/ForbiddenPage.tsx
@@ -1,15 +1,16 @@
 import {useNavigate} from "react-router-dom";
+import Button from "@/shared/components/Button.tsx";
 
 const ForbiddenPage = () => {
     const navigate = useNavigate();
-    // const handleBackToHome = () => {
-    //     navigate("/");
-    // }
+    const handleBackToHome = () => {
+        navigate("/");
+    }
     return (
-        <div className={"min-h-screen flex flex-col items-center justify-center"}>
+        <div className={"min-h-screen flex flex-col items-center justify-center gap-4 max-w-lg mx-auto"}>
             <h1 className="text-6xl font-bold text-error">403</h1>
-            <p className="mt-4 text-lg">You don’t have permission to access this page.</p>
-            {/*<Button variant={"secondary"} onClick={handleBackToHome}>Go back home</Button>*/}
+            <p className="text-lg">You don’t have permission to access this page.</p>
+            <Button variant={"secondary"} onClick={handleBackToHome}>Go back home</Button>
         </div>
     );
 };
