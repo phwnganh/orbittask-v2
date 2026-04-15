b96340e1beb123e3d2503afbb119e854fa3db705
phwnganh <phuonganh25102003@gmail.com>
2026-04-15 09:02:15 +0700

    fix: the color of the header title text

--- src/layouts/components/header/HeaderTitle.tsx
+++ src/layouts/components/header/HeaderTitle.tsx
@@ -1,8 +1,11 @@
+import {useProfile} from "@/features/profile/hooks/useProfile.ts";
+
 const HeaderTitle = () => {
+    const {data: profile} = useProfile()
   return (
-    <div className={"flex items-center gap-1"}>
+    <div className={"flex items-center gap-2"}>
       <h1 className={"font-bold text-xl text-text-secondary"}>Hello, </h1>
-      <span className={"text-lg "}></span>
+      <span className={"text-lg text-text-primary font-black"}>{profile?.last_name}</span>
     </div>
   );
 };
