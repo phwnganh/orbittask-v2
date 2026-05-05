355056904576ddc87fa154d7acfb143b47b9be7c
phwnganh <phuonganh25102003@gmail.com>
2026-05-05 17:44:13 +0700

    fix: not display the page which has not any project items

--- src/features/project/components/sections/ProjectPaginationSection.tsx
+++ src/features/project/components/sections/ProjectPaginationSection.tsx
@@ -1,9 +1,18 @@
 import {useProjectFilterStore} from "@/features/project/stores/project-filter.store.ts";
 import Pagination from "@/shared/components/Pagination.tsx";
 import type {ProjectResponse} from "@/features/project/types/project.type.ts";
+import {useEffect} from "react";
 
 const ProjectPaginationSection = ({projects}: {projects?: ProjectResponse}) => {
     const {page, pageSize, setPage} = useProjectFilterStore()
+
+    const totalPages = Math.ceil((projects?.total ?? 0) / pageSize);
+
+    useEffect(() => {
+        if(page > totalPages && totalPages > 0){
+            setPage(totalPages)
+        }
+    }, [page, totalPages, setPage])
     console.log({
         total: projects?.total,
         pageSize,
