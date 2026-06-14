a2bbc6045972be9703853180ada4376ff1b4a243
phwnganh <phuonganh25102003@gmail.com>
2026-06-14 09:52:13 +0700

    fix: use the shared btn when clicking previous and following month calendar

--- src/features/my-tasks/components/uis/calendars/calendar-section/CalendarHeader.tsx
+++ src/features/my-tasks/components/uis/calendars/calendar-section/CalendarHeader.tsx
@@ -1,6 +1,7 @@
 import {addMonths, format, subMonths} from "date-fns";
 import type {Dispatch, SetStateAction} from "react";
 import ChevronIcon from '@/assets/icons/chevron-right-icon.svg?react'
+import Button from "@/shared/components/button/Button.tsx";
 type CalendarHeaderProps = {
     currentMonth: Date;
     setCurrentMonth: Dispatch<SetStateAction<Date>>;
@@ -8,21 +9,20 @@ type CalendarHeaderProps = {
 const CalendarHeader = ({currentMonth, setCurrentMonth}: CalendarHeaderProps) => {
     return (
         <div className={"flex items-center justify-between mb-6"}>
-            <button
+            <Button type={"button"} fullWidth={false} variant={"secondary"}
                 onClick={() => setCurrentMonth((prev) => subMonths(prev, 1))}
-                className={"p-2 rounded-xl hover:bg-bg-tertiary transition"}
             >
                 <ChevronIcon className={"w-6 h-6 shrink-0 rotate-180"}/>
-            </button>
+            </Button>
             <h2 className={"font-semibold text-lg"}>
                 {format(currentMonth, "MMMM yyyy")}
             </h2>
-            <button
+            <Button type={"button"} fullWidth={false} variant={"secondary"}
                 onClick={() => setCurrentMonth((prev) => addMonths(prev, 1))}
                 className={"p-2 rounded-xl hover:bg-bg-tertiary transition"}
             >
                 <ChevronIcon className={"w-6 h-6 shrink-0"}/>
-            </button>
+            </Button>
         </div>
     );
 };
